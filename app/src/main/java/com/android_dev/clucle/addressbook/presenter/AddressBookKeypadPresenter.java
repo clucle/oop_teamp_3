24b88022add1006e32e1f5b79e2188066c319b6f
clucle <wjdenwls123@gmail.com>
2017-05-09 03:05:01 +0900

    feat: update keypad fragment
    key press show number xxx-xxxx-xxxx form

--- app/src/main/java/com/android_dev/clucle/addressbook/presenter/AddressBookKeypadPresenter.java
+++ app/src/main/java/com/android_dev/clucle/addressbook/presenter/AddressBookKeypadPresenter.java
@@ -6,7 +6,7 @@ public class AddressBookKeypadPresenter {
 
     /* View Method */
     public interface View {
-
+        void showNumber(String text);
     }
 
     private String keyText;
@@ -18,6 +18,27 @@ public class AddressBookKeypadPresenter {
     }
 
     public void addText(String text) {
+        if (keyText.length() > 10) return ;
+        keyText += text;
+        setFormNumber();
+    }
+    public void delText() {
+        keyText = "";
+        setFormNumber();
+    }
+
+    public void setFormNumber() {
+        int lenText = keyText.length();
+        if (lenText < 4) {
+            view.showNumber(keyText);
+            return ;
+        }
+        if (lenText < 8) {
+            view.showNumber(keyText.substring(0,3) + "-" + keyText.substring(3));
+            return ;
+        }
+        view.showNumber(keyText.substring(0,3) + "-" + keyText.substring(3, lenText - 4)
+                        + "-" + keyText.substring(lenText - 4));
 
     }
 }
--- app/src/main/java/com/android_dev/clucle/addressbook/view/fragment/AddressBookKeypadFragment.java
+++ app/src/main/java/com/android_dev/clucle/addressbook/view/fragment/AddressBookKeypadFragment.java
@@ -10,21 +10,28 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android_dev.clucle.addressbook.R;
+import com.android_dev.clucle.addressbook.presenter.AddressBookKeypadPresenter;
+
+import org.w3c.dom.Text;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import butterknife.Unbinder;
 
-public class AddressBookKeypadFragment extends Fragment{
+public class AddressBookKeypadFragment extends Fragment implements AddressBookKeypadPresenter.View{
     public AddressBookKeypadFragment() {
         super();
     }
 
     private Unbinder unbinder;
+    private AddressBookKeypadPresenter keypadPresenter;
+    @BindView(R.id.text_show_number)
+    TextView text_show_number;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -35,6 +42,7 @@ public class AddressBookKeypadFragment extends Fragment{
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_addressbook_keypad, container, false);
+        keypadPresenter = new AddressBookKeypadPresenter(this);
         unbinder = ButterKnife.bind(this, view);
         return view;
     }
@@ -45,9 +53,54 @@ public class AddressBookKeypadFragment extends Fragment{
         unbinder.unbind();
     }
 
-    @OnClick(R.id.btn_keypad_num1)
+    @Override
+    public void showNumber(String text) {
+        text_show_number.setText(text);
+    }
+
+    @OnClick({R.id.btn_keypad_num1, R.id.btn_keypad_num2, R.id.btn_keypad_num3,
+            R.id.btn_keypad_num4, R.id.btn_keypad_num5, R.id.btn_keypad_num6,
+            R.id.btn_keypad_num7, R.id.btn_keypad_num8, R.id.btn_keypad_num9,
+            R.id.btn_keypad_star, R.id.btn_keypad_num0, R.id.btn_keypad_shap})
     public void onKeyClick(View view) {
-        Toast.makeText(getActivity(), "Working", Toast.LENGTH_LONG).show();
+        switch (view.getId()) {
+            case R.id.btn_keypad_num1:
+                keypadPresenter.addText("1");
+                break;
+            case R.id.btn_keypad_num2:
+                keypadPresenter.addText("2");
+                break;
+            case R.id.btn_keypad_num3:
+                keypadPresenter.addText("3");
+                break;
+            case R.id.btn_keypad_num4:
+                keypadPresenter.addText("4");
+                break;
+            case R.id.btn_keypad_num5:
+                keypadPresenter.addText("5");
+                break;
+            case R.id.btn_keypad_num6:
+                keypadPresenter.addText("6");
+                break;
+            case R.id.btn_keypad_num7:
+                keypadPresenter.addText("7");
+                break;
+            case R.id.btn_keypad_num8:
+                keypadPresenter.addText("8");
+                break;
+            case R.id.btn_keypad_num9:
+                keypadPresenter.addText("9");
+                break;
+            case R.id.btn_keypad_star:
+                keypadPresenter.addText("*");
+                break;
+            case R.id.btn_keypad_num0:
+                keypadPresenter.addText("0");
+                break;
+            case R.id.btn_keypad_shap:
+                keypadPresenter.delText();
+                break;
+        }
     }
 
 
