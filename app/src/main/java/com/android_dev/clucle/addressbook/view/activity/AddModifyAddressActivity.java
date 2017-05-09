3e0b4e47202ce47f5adaee5ce6d0f5616c318df3
clucle <wjdenwls123@gmail.com>
2017-05-09 17:35:18 +0900

    feat: make new activity add_modify_activity
    new activity, manifest setting

---
+++ app/src/main/java/com/android_dev/clucle/addressbook/view/activity/AddModifyAddressActivity.java
@@ -0,0 +1,7 @@
+package com.android_dev.clucle.addressbook.view.activity;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class AddModifyAddressActivity  extends AppCompatActivity {
+
+}
