ec8d0a9c3af1fc27ba3a03650e43198edf336032
clucle <wjdenwls123@gmail.com>
2017-05-11 15:19:29 +0900

    feat: backPressedEvent

--- app/src/main/java/com/android_dev/clucle/addressbook/view/activity/AddModifyAddressActivity.java
+++ app/src/main/java/com/android_dev/clucle/addressbook/view/activity/AddModifyAddressActivity.java
@@ -3,14 +3,64 @@ package com.android_dev.clucle.addressbook.view.activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.android_dev.clucle.addressbook.R;
+import com.android_dev.clucle.addressbook.utils.BackPressCloseHandler;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class AddModifyAddressActivity extends AppCompatActivity {
+    @BindView(R.id.imageButton_character) ImageButton imageButtonCharacter;
+    @BindView(R.id.editText_add_modify_name) EditText editTextName;
+    @BindView(R.id.editText_add_modify_number) EditText editTextNumber;
+    @BindView(R.id.editText_add_modify_club) EditText editTextClub;
+    @BindView(R.id.editText_add_modify_email) EditText editTextEmail;
+
+    private int identifyCharacter;
+    private BackPressCloseHandler backPressCloseHandler;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_modify_address);
+        ButterKnife.bind(this);
+        identifyCharacter = 1;
+        backPressCloseHandler = new BackPressCloseHandler(this);
+    }
+
+    @OnClick(R.id.imageButton_character)
+    public void onClickCharacter() {
+        if (identifyCharacter == 1) {
+            imageButtonCharacter.setImageResource(R.drawable.img_btn_blue_dog);
+            identifyCharacter = 2;
+        } else {
+            imageButtonCharacter.setImageResource(R.drawable.img_btn_pink_dog);
+            identifyCharacter = 1;
+        }
+    }
+
+    @OnClick(R.id.text_save)
+    public void onClickSave() {
+        String name = editTextName.getText().toString().trim();
+        String number = editTextNumber.getText().toString().trim();
+        String club = editTextClub.getText().toString().trim();
+        String editTextEmail = editTextName.getText().toString().trim();
+
+        if (name.length() == 0) {
+            Toast.makeText(getApplicationContext(), "이름은 공백으로 저장 할 수 없습니다.", Toast.LENGTH_LONG).show();
+            return ;
+        }
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        backPressCloseHandler.onBackPressed();
     }
 }
