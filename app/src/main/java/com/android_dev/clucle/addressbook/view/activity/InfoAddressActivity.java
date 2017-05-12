8057d63495f17f8371ab2f7a1a4aeef60a6299e9
clucle <wjdenwls123@gmail.com>
2017-05-13 00:22:22 +0900

    feat: update modify info UI

---
+++ app/src/main/java/com/android_dev/clucle/addressbook/view/activity/InfoAddressActivity.java
@@ -0,0 +1,15 @@
+package com.android_dev.clucle.addressbook.view.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.android_dev.clucle.addressbook.R;
+
+public class InfoAddressActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info_address);
+
+    }
+}
