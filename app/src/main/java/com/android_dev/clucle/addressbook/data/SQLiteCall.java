71f5a2941b36c5bb7b8dd3d57636e5c88667cc02
clucle <wjdenwls123@gmail.com>
2017-05-15 16:38:16 +0900

    feat: make simple call db

---
+++ app/src/main/java/com/android_dev/clucle/addressbook/data/SQLiteCall.java
@@ -0,0 +1,38 @@
+package com.android_dev.clucle.addressbook.data;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class SQLiteCall extends SQLiteOpenHelper{
+    private String Table = "calltest";
+
+    public SQLiteCall(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE if not exists " + Table +
+                " (type STRING, number STRING, datetime DATETIME DEFAULT CURRENT_TIMESTAMP)"
+        );
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        throw new UnsupportedOperationException("not implemented");
+    }
+
+    public void insert(String type, String number, String datetime) {
+        ContentValues values = new ContentValues();
+        values.put("type", type);
+        values.put("number", number);
+        values.put("datetime", datetime);
+        this.getWritableDatabase().insert(Table, null, values);
+    }
+
+    public void delete (String name) {
+        this.getWritableDatabase().delete(Table, "name=?", new String[]{name});
+    }
+}
